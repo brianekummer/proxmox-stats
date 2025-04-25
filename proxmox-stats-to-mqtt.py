d1401c0275f201a21a728c8b1c1a5f1f28dcac8f
Brian Kummer <brianekummer@users.noreply.github.com>
2025-04-25 12:51:59 -0400

    Minor cleanup

--- proxmox-stats-to-mqtt.py
+++ proxmox-stats-to-mqtt.py
@@ -25,6 +25,8 @@ from datetime import datetime, timezone
 load_dotenv()
 PROXMOX_HOST = os.getenv("PROXMOX_HOST")
 PROXMOX_NODE = os.getenv("PROXMOX_NODE")
+PROXMOX_NAS_STORAGE_NAME = os.getenv("PROXMOX_NAS_STORAGE_NAME")
+
 API_USER = os.getenv("API_USER")
 API_REALM = os.getenv("API_REALM")
 API_TOKEN_ID = os.getenv("API_TOKEN_ID")
@@ -52,6 +54,22 @@ session.headers.update({
 })
 
 
+# ====== CONVERSION FUNCTIONS ======
+# unit | base | 1 unit equals x bytes
+# -----|------|----------------------
+# MiB  | 2    | 1,048,576
+# GiB  | 2    | 1,073,741,824
+# TiB  | 2    | 1,099,511,627,776
+# MB   | 10   | 1,000,000
+# GB   | 10   | 1,000,000,000
+# TB   | 10   | 1,000,000,000,000
+bytes_to_mib = lambda num_bytes: num_bytes / (1024 * 1024)
+bytes_to_gib = lambda num_bytes: num_bytes / (1024 * 1024 * 1024)
+bytes_to_gb = lambda num_bytes: num_bytes / (1000 * 1000 * 1000)
+bytes_to_tb = lambda num_bytes: num_bytes / (1000 * 1000 * 1000 * 1000)
+# ====== END CONVERSION FUNCTIONS ======
+
+
 def parse_args():
     parser = argparse.ArgumentParser(description="Proxmox Stats to MQTT")
     parser.add_argument('--publish-discovery', action='store_true', help="Publish MQTT discovery messages")
@@ -59,61 +77,53 @@ def parse_args():
 
 
 def get_json(path):
+    """
+    Gets JSON data from the Proxmox API
+    """
     url = f"https://{PROXMOX_HOST}:8006/api2/json{path}"
     response = session.get(url)
     response.raise_for_status()
     return response.json()["data"]
 
 
-def get_all_vms():
+def get_friendly_name(vmid, name, vm_type):
     """
-    Retrieves all VMs and containers from all Proxmox nodes,
-    explicitly adding a 'type' field to each for consistency.
+    Get a friendly name for the sensor based on the VM type and ID
 
-    Assumes a single Proxmox node for simplicity of code
+    Examples:
+      name           | vmid | vm_type | friendly_name
+      ---------------|------|---------|------------------
+      home-assistant | 100  | qemu    | Home Assistant VM
+      (none)         | 101  | lxc     | Lxc 101 LXC
+    """
+    friendly_name = name or f"{vm_type} {vmid}"
+    return f"{friendly_name.replace('_', ' ').replace('-', ' ').title()} {'VM' if vm_type == 'qemu' else 'LXC'}"
+
+
+def get_all_vms(proxmox_node):
+    """
+    Retrieves all VMs and containers from the specified Proxmox node,
+    explicitly adding a 'type' field to simply code later
     """
     all_vms = []
-    for vm in get_json(f"/nodes/{PROXMOX_NODE}/qemu"):
+    for vm in get_json(f"/nodes/{proxmox_node}/qemu"):
         vm["type"] = "qemu"
         all_vms.append(vm)
 
-    for vm in get_json(f"/nodes/{PROXMOX_NODE}/lxc"):
+    for vm in get_json(f"/nodes/{proxmox_node}/lxc"):
         vm["type"] = "lxc"
         all_vms.append(vm)
 
     return all_vms
 
 
-def bytes_to_mib(num_bytes):
-    # MB is a base-10 unit, 1 MB equals 1,000,000 bytes
-    # MiB is a base-2 unit. 1 MiB equals 1,048,576 bytes
-    return num_bytes / (1024 * 1024)
-
-def bytes_to_gib(num_bytes):
-    # GB uses the decimal system, 1 GB equals 1,000,000,000 bytes
-    # GiB uses the binary system, 1 GiB equals 1,073,741,824 bytes
-    return num_bytes / (1024 * 1024 * 1024)
-
-def bytes_to_gb(num_bytes):
-    # GB uses the decimal system, 1 GB equals 1,000,000,000 bytes
-    # GiB uses the binary system, 1 GiB equals 1,073,741,824 bytes
-    return num_bytes / (1000 * 1000 * 1000)
-
-def bytes_to_tb(num_bytes):
-    # TB uses the decimal system, 1 TB equals 1,000,000,000,000 bytes
-    # TiB uses the binary system, 1 TiB equals 1,099,511,627,776 bytes
-    return num_bytes / (1000 * 1000 * 1000 * 1000)
-
-
-def get_friendly_name(vmid, name, vm_type):
-    """
-    Get a friendly name for the sensor based on the VM type and ID
-    """
-    friendly_name = name or f"{vm_type} {vmid}"
-    return f"{friendly_name.replace('_', ' ').replace('-', ' ').title()} {'VM' if vm_type == 'qemu' else 'LXC'}"
-
-
 def get_vm_disk_usage(ssh_host, ssh_username, ssh_key_path):
+    """
+    Get the disk usage of a VM
+
+    The Proxmox API can't get the disk usage of a VM, so we have to SSH into the VM to get it
+    by running the df command and parsing the output
+    """
     ssh = paramiko.SSHClient()
     ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
 
@@ -130,16 +140,19 @@ def get_vm_disk_usage(ssh_host, ssh_username, ssh_key_path):
 
 
 def get_host_stats(vms):
-    # Get host node stats
+    """
+    Get the stats for the Proxmox host
+
+    This is a work-in-progress, since I am not yet sure exactly what data I want
+    """
     node_status = get_json(f"/nodes/{PROXMOX_NODE}/status")
-    #print(json.dumps(node_status, indent=2))
     
     # Total host memory
-    #   - This is how much of the host's memory that is currently available, excluding the memory
-    #     being used by vms and containers right now
+    #   - This is how much of the host's memory is currently available, excluding the memory
+    #     currently being used by vms and containers right now
     #   - This is NOT the amount of physical memory installed on the host
-    #   - An example: the host has 16gb of physical memory, and all the VMs/containers are currently
-    #     using 3GB (they're allotted 11GB). This shows 13GB.
+    #   - An example: if the host has 16GB of physical memory, and all the VMs/containers are allotted 11GB, 
+    #     but are currently using 3GB, then this shows 13GB.
     total_host_mem = node_status["memory"]["total"]
     
     used_mem = node_status["memory"]["used"]
@@ -168,7 +181,9 @@ def get_host_stats(vms):
             "device_model": "Proxmox Host",
             "friendly_name": "Proxmox Host",
 
-            # I need this in collect_stats(), so I'll return this here
+            # In collect_stats(), I want to know what percentage of the host memory is used by
+            # each VMs/container, so I need to pass this back so it is available for that
+            # calculation
             "total_host_mem": total_host_mem,
 
             "state_topic_prefix": f"{MQTT_TOPIC_PREFIX}/proxmox_host",
@@ -179,7 +194,7 @@ def get_host_stats(vms):
 
                 # TODO LATER- I'm so confused !!!!
                 #
-                # I want memory to be what's not allocated to VMs, not the total memory of the host
+                # I THINK want memory to be what's not allocated to VMs, not the total memory of the host
                 # I have 16GB of memory, 11 GB is allotted to vms and containers, I want this to show 5 GB
                 # But Proxmox says the total memory of the host is 13GB (apparently Proxmox, etc is using 3GB of memory),
                 # so that means I have ~ 1.5GB - 2GB of memory for the Proxmox host if all the containers max out
@@ -197,25 +212,39 @@ def get_host_stats(vms):
     ]
 
 
-def get_vm_stats(vmid, vm_type, total_host_mem):
-    vm_status = get_json(f"/nodes/{PROXMOX_NODE}/{vm_type}/{vmid}/status/current")
+def get_vm_stats(proxmox_node, vmid, vm_type, total_host_mem):
+    """
+    Get the stats for a VM or container
+
+    Oddities - The Proxmox API...
+      - Returns uptime as second (i.e. it's been up for 625,536 seconds)
+        and we convert it to an HA timestamp that is the date/time of the last boot,
+        such as 2023-10-01T12:00:00Z
+      - The 
+      - Cannot get the disk usage of a VM- it always returns 0%. So I have a function
+        that SSH's into the VM and runs a linux command to get the disk usage percentage
+          - This is configured by setting environment variables for each VM, with the 
+            VM's vmid in the variable name, with the necessary parameters. Example:
+              SSH_HOST_QEMU_100 = "192.168.1.193"
+              SSH_USERNAME_QEMU_100 = "hassio"
+              SSH_KEY_PATH_QEMU_100 = "/root/.ssh/id_rsa_"
+    """
+    vm_status = get_json(f"/nodes/{proxmox_node}/{vm_type}/{vmid}/status/current")
 
     sensor_key_prefix = f"proxmox_{vm_type}_{vmid}"
 
     mem_alloc = vm_status.get("maxmem", 0)
     mem_used = vm_status.get("mem", 0)
+    cpus = vm_status.get("cpus", 0)
+    disk_alloc_gb = math.ceil(bytes_to_gib(vm_status.get("maxdisk", 0)))
+
+    # Convert uptime to last boot time
     uptime_seconds = vm_status.get("uptime", 0)
     last_boot_time = int(time.time()) - int(uptime_seconds)
     last_boot_time_iso = datetime.fromtimestamp(last_boot_time, tz=timezone.utc).isoformat()
-    cpus = vm_status.get("cpus", 0)
 
-    # Yes, this conversion is weird, but 32GB can be 32-34GB depending on how you do the math,
-    # and doing it this way yields 32GB- convert to GiB and then round up.
-    disk_alloc_gb = math.ceil(bytes_to_gib(vm_status.get("maxdisk", 0)))
-
-    # The Proxmox API can't get the disk usage of a VM, so we have to SSH into the VM to get it
-    # Environment variables are set in the .env file for each VM and include the vmid
-    if vmid == 100:
+    # Disk used percent is different for VMs and containers
+    if vm_type == "qemu":
         ssh_host = os.getenv(f"SSH_HOST_QEMU_{vmid}")
         ssh_username = os.getenv(f"SSH_USERNAME_QEMU_{vmid}")
         ssh_keypath = os.getenv(f"SSH_KEY_PATH_QEMU_{vmid}")
@@ -224,6 +253,7 @@ def get_vm_stats(vmid, vm_type, total_host_mem):
         disk_used_gb = math.ceil(bytes_to_gib(vm_status.get("disk", 0)))
         vm_disk_used_percent = round(disk_used_gb / disk_alloc_gb * 100, 2) if disk_alloc_gb else 0
 
+    # Build and return the data
     return {
         "friendly_name": get_friendly_name(vmid, vm_status.get("name"), vm_type),
         "device_id": sensor_key_prefix,
@@ -242,6 +272,14 @@ def get_vm_stats(vmid, vm_type, total_host_mem):
 
 
 def get_nas_stats(storage_data, storage_name):
+    """
+    Get the stats for a disk storage
+    
+    I have an external hard drive attached to my server and a container serving that up as a NAS.
+
+    Yes, using TB/GB (decimal/1000) instead of TiB/GiB (binary/1024) here is inconsistent, but it
+    matches the Proxmox dashboard and the stated capacity of the NAS (6 TB)
+    """
     storage = next((s for s in storage_data if s.get("storage") == storage_name), None)
     if storage:
         return [
@@ -251,8 +289,6 @@ def get_nas_stats(storage_data, storage_name):
                 "friendly_name": "NAS",
                 "state_topic_prefix": f"{MQTT_TOPIC_PREFIX}/proxmox_nas",
                 "stats": {
-                    # Yes, using TB/GB (decimal/1000) instead of TiB/GiB (binary/1024) here is inconsistent, but it matches
-                    # the Proxmox dashboard and the stated capacity of the NAS (6 TB)
                     "proxmox_nas_disk_size_tb": round(bytes_to_tb(storage.get("total", 0)), 2),
                     "proxmox_nas_disk_used_gb": round(bytes_to_gb(storage.get("used", 0)), 2),
                     "proxmox_nas_disk_used_percent": round(storage.get("used_fraction", 0) * 100, 1)
@@ -265,29 +301,27 @@ def get_nas_stats(storage_data, storage_name):
 def collect_stats():
     """
     Collect stats from the Proxmox API and structure them for MQTT publishing
+
+    The code assumes there is only one Proxmox node
     """
-    vms = get_all_vms()
+    vms = get_all_vms(PROXMOX_NODE)
     stats = {
         "host": get_host_stats(vms),
         "vms": [],
-        "nas": get_nas_stats(get_json(f"/nodes/{PROXMOX_NODE}/storage"), "nas-public")
+        "nas": get_nas_stats(get_json(f"/nodes/{PROXMOX_NODE}/storage"), PROXMOX_NAS_STORAGE_NAME)
     }
 
-    # Get host stats
-    stats["host"] = get_host_stats(vms)
-
-    # Get VM/container stats
-    stats["vms"] = []
     for vm in vms:
-        stats["vms"].append(get_vm_stats(vm["vmid"], vm["type"], stats["host"][0]["total_host_mem"]))
-
-    # Get NAS stats
-    stats["nas"] = get_nas_stats(get_json(f"/nodes/{PROXMOX_NODE}/storage"), "nas-public")
+        vm_stats = get_vm_stats(PROXMOX_NODE, vm["vmid"], vm["type"], stats["host"][0]["total_host_mem"])
+        stats["vms"].append(vm_stats)
 
     return stats
 
 
 def publish_all_stats_to_mqtt(stats):
+    """
+    Publish all stats to MQTT
+    """
     print(f"Publishing stats to MQTT at {datetime.now()}")
     client = mqtt.Client()
     client.username_pw_set(MQTT_USERNAME, MQTT_PASSWORD)
@@ -321,7 +355,7 @@ def publish_discovery_message(client, sensor_key, name, state_topic, device_info
         "state_class": "measurement"
     }
 
-    # Add device_class only if it's not None
+    # Add device_class and icon only if they're not None
     if device_class:
         payload["device_class"] = device_class
     if icon:
@@ -337,7 +371,7 @@ def publish_discovery_message(client, sensor_key, name, state_topic, device_info
 
 def publish_sensor_discovery_by_device(client, device_id, device_model, device_friendly_name, state_topic_prefix, device_stats):
     """
-    Publish discovery messages for a set of sensors.
+    Publish discovery messages for a set of sensors
     """
     sensor_definitions = [
         # Common sensors
@@ -385,7 +419,7 @@ def publish_sensor_discovery_by_device(client, device_id, device_model, device_f
 
 def publish_discovery_messages(stats):
     """
-    Publish MQTT discovery messages to Home Assistant for the Proxmox host, NAS, and all VMs/containers
+    Publish MQTT discovery messages to Home Assistant for all devices (host, VMs, containers, NAS)
     """
     client = mqtt.Client()
     client.username_pw_set(MQTT_USERNAME, MQTT_PASSWORD)
@@ -399,19 +433,9 @@ def publish_discovery_messages(stats):
 
 
 
-
 # Main script execution
 if __name__ == "__main__":
     args = parse_args()
-
-
-
-    # TODO- While in dev
-    # for HA vm, this will replace proxmox_qemu_100_disk_used_percent
-    #ha_disk_used_pct = get_vm_disk_usage(SSH_HOST, SSH_USERNAME)
-    #print(f"HA Disk Used: {ha_disk_used_pct} %")
-
-
 
     stats = collect_stats()
     publish_all_stats_to_mqtt(stats)
@@ -420,4 +444,3 @@ if __name__ == "__main__":
         publish_discovery_messages(stats)
 
     print("Stats published to MQTT.")
-
--- run.sh
+++ run.sh
@@ -1,3 +1,27 @@
 #!/bin/bash
-source venv/bin/activate
-python proxmox-stats-to-mqtt-2.py
+case "$OSTYPE" in
+  linux-gnu*)
+    ##########################################################################
+    # Proxmox server
+    ##########################################################################
+    cd /root/proxmox-stats
+
+    # Load the Python virtual environment
+    source venv/bin/activate
+
+    # Load .env variables
+    export $(grep -v '^#' .env | xargs)
+
+    # Run the script
+    python proxmox-stats-to-mqtt.py
+    ;;
+
+  msys)
+    ##########################################################################
+    # Windows, running in Git Bash
+    ##########################################################################
+    #source venv/bin/activate
+    source .env
+    python proxmox-stats-to-mqtt.py
+    ;;
+esac
